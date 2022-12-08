b431a1de25c51b2bba215e9caa135eba93854b77
Your Name <you@example.com>
2022-12-08 08:37:19 +0530

    Added: lambda Expression

---
+++ src/main/java/com/bridgelabz/MathOperation.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+@FunctionalInterface
+interface IMathFunction {
+    int calculate(int a,int b);
+}
+
+public class MathOperation {
+
+    public static void main(String[] args) {
+        IMathFunction add = Integer :: sum;
+        IMathFunction multiply = (int a,int b) -> (a*b);
+        IMathFunction divide = (int a,int b) -> (a/b);
+
+        System.out.println("Addition = "+add.calculate(4,2));
+        System.out.println("Multiplication = "+multiply.calculate(4,2));
+        System.out.println("Division = "+divide.calculate(4,2));
+    }
+}
