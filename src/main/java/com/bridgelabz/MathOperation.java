5c3d4f8fc0df87f0c645943327ede1e3b68e635f
Your Name <you@example.com>
2022-12-08 11:33:48 +0530

    Added: Perform math functions

--- src/main/java/com/bridgelabz/MathOperation.java
+++ src/main/java/com/bridgelabz/MathOperation.java
@@ -8,12 +8,12 @@ interface IMathFunction {
 public class MathOperation {
 
     public static void main(String[] args) {
-        IMathFunction add = Integer :: sum;
-        IMathFunction multiply = (int a,int b) -> (a*b);
-        IMathFunction divide = (int a,int b) -> (a/b);
+        IMathFunction add = Integer::sum;
+        IMathFunction multiply = (int a, int b) -> (a * b);
+        IMathFunction divide = (int a, int b) -> (a / b);
 
-        System.out.println("Addition = "+add.calculate(4,2));
-        System.out.println("Multiplication = "+multiply.calculate(4,2));
-        System.out.println("Division = "+divide.calculate(4,2));
+        System.out.println("Addition = " + add.calculate(4, 2));
+        System.out.println("Multiplication = " + multiply.calculate(4, 2));
+        System.out.println("Division = " + divide.calculate(4, 2));
     }
 }
